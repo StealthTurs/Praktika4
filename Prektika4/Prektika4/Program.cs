75364148160087505fe78890bcd11db6e191b8d8
student <student@C251U49>
2020-09-25 18:51:27 +0300

    плщварыл

---
+++ Prektika4/Prektika4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace prakt4_01
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      ArrayList myList = new ArrayList() { "EGOR" };
+      myList.Add("DIMA");
+      myList.Add("ARTEM");
+      myList.Add("HENESSY");
+      myList.Add("assembler");
+      foreach (string item in myList)
+      {
+        Console.WriteLine("Unsorted: {0}", item);
+      }
+      
+      myList.Sort();
+
+      foreach (string item in myList)
+      {
+        Console.WriteLine("   Sorted: {0}", item);
+      }
+
+    }
+
+  }
+}
